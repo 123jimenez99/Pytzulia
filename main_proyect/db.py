0a8b3de61d1a3b67bd62c448710ec9e264775df5
Asier Jiménez <123jimenez99@gmail.com>
2023-03-14 17:47:58 +0100

    Rename files

---
+++ main_proyect/db.py
@@ -0,0 +1,31 @@
+import time
+import mysql.connector
+
+mydb = mysql.connector.connect(
+host="localhost",
+user="pico",
+password="sanluis",
+database="test"
+)
+
+# Crea el cursor para poder acceder y manipular datos en la base de datos (Simulando que hemos pasado una tarjeta RFID)
+mycursor = mydb.cursor()
+
+# Crea la tabla en la que se van a pasar los datos de tiempo
+mycursor.execute("CREATE TABLE IF NOT EXISTS rfid_record (rfid VARCHAR(64), timestamp DATETIME(2))")
+
+# Comprueba continuamente si ha pasado una tarjeta RFID
+while True:
+    rfid = input("Esperando tarjeta RFID: ")
+
+# Consigue tiempo actual del sistema
+    kernel_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+# Manda el tiempo actual del sistema a la base de datos
+    sql = "INSERT INTO rfid_record (rfid, timestamp) VALUES (%s, %s)"
+    val = (rfid, kernel_time)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+# Notifica de que todo ha funcionado correctamente
+    print("Datos escaneados y enviados correctamente")
